f89d018ac791f66442c6586a910d34bc360be8a9
Shehan Peiris <shehanmaleesha4884@gmail.com>
2024-10-21 15:45:08 +0530

    re arranged

---
+++ src/test/java/com/example/validdator/validation/CardMapperTest.java
@@ -0,0 +1,29 @@
+package com.example.validdator.validation;
+
+import com.example.validdator.CardMapper;
+import com.example.validdator.dtos.CardDTO;
+import com.example.validdator.model.Card;
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class CardMapperTest {
+
+
+    CardMapper cardMapper = new CardMapper();
+    @Test
+    public void testMapCart(){
+        Card tCart = new Card("1234567890987654", "12/23", "shehan maleesha");
+
+
+        CardDTO tcardDto = cardMapper.apply(tCart);
+// new comment added
+        assertEquals(tCart.getCardHolderName(),  tcardDto.cardHolderName());
+
+    }
+
+}
